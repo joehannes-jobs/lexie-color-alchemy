9bd30b958ef88e5d7122f29a5c8c0483cec523dc
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2022-12-19 13:57:23 -0400

    feat(component): basic non droppable source-tile

---
+++ color-alchemy-frontend/src/components/atoms/Source.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from "react";
+
+import { Tile } from "./Tile";
+import { TColor } from "../../app/types";
+
+interface ISourceProps {
+  color: TColor;
+}
+
+/*
+ * @description - Base Component for Sources
+ * @param color - the current color of the source
+ * @returns the React Component Markup
+ */
+export const Source: FC<ISourceProps> = ({ color = { r: 0, g: 0, b: 0 } }) => {
+  return <Tile color={color} classNames="rounded-full" />;
+};
