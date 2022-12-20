b74b448952896c9efe390726e4cdb63e97fdefa3
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2022-12-20 12:07:36 -0400

    feat(component): basic Game component with headers

--- color-alchemy-frontend/src/components/templates/Game.tsx
+++ color-alchemy-frontend/src/components/templates/Game.tsx
@@ -1,12 +1,21 @@
 import React, { useState, useEffect, FC } from "react";
 import classnames from "classnames";
 
+import {
+  Title,
+  UserHeading,
+  MovesHeading,
+  TargetHeading,
+  ClosestColorHeading,
+} from "../molecules/Heading";
+
 import { useAppSelector, useAppDispatch } from "../../app/hooks";
 import {
   fetchGameAsync,
   step,
   selectGame,
-  selectClosestTile,
+  selectClosestColor,
+  IGameState,
 } from "../../features/game/gameSlice";
 import { TColor, TColorComponent } from "../../app/types";
 
@@ -20,8 +29,29 @@ export const Game = () => {
     gameBoardTiles: tiles,
     gameBoardSources: sources,
     target: targetColor,
-  } = useAppSelector(selectGame);
+  }: Partial<IGameState> = useAppSelector(selectGame);
+  const closestColor = useAppSelector(selectClosestColor);
   const dispatch = useAppDispatch();
 
-  return <main></main>;
+  useEffect(() => {
+    dispatch(fetchGameAsync(userId));
+  }, []);
+
+  return (
+    <main className="inline-flex flex-col mx-auto leading-810 gap-2">
+      <Title />
+      <UserHeading userId={userId as unknown as Pick<IGameState, "userId">} />
+      <MovesHeading
+        maxMoves={moves as unknown as Pick<IGameState, "maxMoves">}
+      />
+      <TargetHeading
+        targetColor={{
+          r: targetColor[0] ?? 0,
+          g: targetColor[1] ?? 0,
+          b: targetColor[2] ?? 0,
+        }}
+      />
+      <ClosestColorHeading closestColor={closestColor as TColor} />
+    </main>
+  );
 };
